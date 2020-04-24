e5001682013e2d937549767d46091eb918a56a88
luanesouza <luanesouza411@gmail.com>
2020-04-24 15:39:10 -0400

    deploy

--- src/services/users.js
+++ src/services/users.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import { api, updateToken } from './api-helper';
 // const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://dry-oasis-20600.herokuapp.com'
+const BASE_URL = 'https://stark-ocean-92228.herokuapp.com'
 
 const loginUser = async (data) => {
   try {
